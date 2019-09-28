f85b7c0e74ec84c8a71f79784a577c5e69dd4cc4
webmad <alecmei.gubin@yandex.ru>
2019-09-28 19:28:26 +0400

    Убрал timestamps из таблицы roles

--- database/migrations/2019_09_22_133434_create_roles_table.php
+++ database/migrations/2019_09_22_133434_create_roles_table.php
@@ -16,7 +16,6 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->timestamps();
         });
     }
 
