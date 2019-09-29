ef7bd7cf1bd68cd950ec4cbc16a01cd736c606dd
WebMad <alecmei.gubin@yandex.ru>
2019-09-29 19:03:17 +0400

    Добавил все необходимые миграции и seeder для project_roles

---
+++ app/ProjectRole.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectRole extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = ['name'];
+
+    public const TUTOR = 1;
+    public const MENTOR = 2;
+    public const LEADER = 3;
+    public const MEMBER = 4;
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
+        $this->call([
+            RolesTableSeeder::class,
+            ProjectRolesSeeder::class,
+        ]);
     }
 }
