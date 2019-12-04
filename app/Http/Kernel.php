288f76fb465007c876912652c5344707276c0c5d
webmad <alecmei.gubin@yandex.ru>
2019-12-04 10:41:36 +0400

    Пофиксил корс, "Изничтожил это"

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,6 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            'cors' => \App\Http\Middleware\Cors::class,
         ],
     ];
 
@@ -63,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 
     /**
